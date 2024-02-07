e9632400ff77d17cd7fd5e5678e2fa5c2bfba250
jgresham <johnsgresham@gmail.com>
2024-02-07 13:47:20 -0800

    deploy to fly works. requires port 3000. add sort by count for table data

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import envOnly from "vite-env-only"
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [remix(), tsconfigPaths(), envOnly()],
 });
