dde52c6b4263e4c6f484f1e029a467c4326f3a71
jgresham <johnsgresham@gmail.com>
2024-07-08 12:15:23 -0400

    warning banner for incorrect data

--- app/Main.tsx
+++ app/Main.tsx
@@ -58,6 +58,7 @@ export default function Main() {
         <Links />
       </head>
       <body className={theme}>
+       <p style={{textAlign: 'center', background: 'red', padding: '10px 0px'}}>This dashboard is under maintenance. The data is currently incorrect.</p>
         <div id="topOfPage"></div>
         <header className={isMinimized ? "minimized" : ""}>
           <div className="logo">
